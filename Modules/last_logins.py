50b116939e67b2d8dc89349c5a21a090e31e9682
keraattin <kerem-oruc@outlook.com>
2024-07-14 14:37:10 +0300

    Delete older than 30 day sessions module added

---
+++ Modules/last_logins.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from datetime import datetime, timedelta
+from db_connection import db
+##############################################################################
+
+
+##############################################################################
+def delete_last_logins():
+    # Access the 'Session' collection
+    collection = db['session']
+
+    # Calculate the date 7 days ago from now
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+
+    # Define the query to find documents with 'Created' date older than 30 days
+    query = {"created": {"$lt": thirty_days_ago}}
+    
+    # Perform the deletion
+    result = collection.delete_many(query)
+    
+    # Print each session
+    print(f"Deleted {result.deleted_count} old sessions.")
+##############################################################################
