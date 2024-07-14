9b91a6652326aeffda6e2df374207b9520331a0a
keraattin <kerem-oruc@outlook.com>
2024-07-14 14:41:36 +0300

    configs added to keep configs

---
+++ configs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+import json
+##############################################################################
+
+# Configs
+##############################################################################
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        config_data = json.load(file)
+    return config_data
+
+# Specify the path to your JSON configuration file
+config_file_path = 'config.json'
+
+# Load configuration from the JSON file
+config = load_config(config_file_path)
+
+# Get Scheduled Jobs from Config
+SCHEDULED_JOBS = config['scheduled_jobs']
+
+# Get DB Configs from Config
+DB_ATTRS = config['database']
+##############################################################################
