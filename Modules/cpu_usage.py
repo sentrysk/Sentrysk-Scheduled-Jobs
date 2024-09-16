f26e9e5811a9bf19568b21a6fd385e04ccc487af
keraattin <kerem-oruc@outlook.com>
2024-09-16 17:59:14 +0300

    CPU usage deletion period config implemented

--- Modules/cpu_usage.py
+++ Modules/cpu_usage.py
@@ -5,6 +5,7 @@
 import logging
 from datetime import datetime, timedelta
 from db_connection import db
+from configs import CPU_USAGE_DEL_PER
 ##############################################################################
 
 
@@ -14,7 +15,7 @@ def delete_cpu_usage_data():
     collection = db['cpu_usage']
 
     # Calculate the date X days ago from now
-    x_days_ago = datetime.now() - timedelta(days=3)
+    x_days_ago = datetime.now() - timedelta(days=CPU_USAGE_DEL_PER)
 
     # Define the query to find documents with 'timestamp' date older than X days
     query = {"timestamp": {"$lt": x_days_ago}}
